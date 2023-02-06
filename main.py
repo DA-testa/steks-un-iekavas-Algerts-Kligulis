e0cf3c5b0306e8ae6d88fde064a2b91da75cb5d8
Algerts-Kligulis <124618375+Algerts-Kligulis@users.noreply.github.com>
2023-02-06 20:05:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,29 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
+    global opening_brackets_stack
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+            # Process opening bracket, write your code her
+            opening_brackets_stack.append(Bracket(next, i + 1))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack)==0 or not(are_matching(opening_brackets_stack[-1].char,next)):
+                print(i+1)
+                return
+            opening_brackets_stack.pop()
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    if len(opening_brackets_stack)!=0:
+        print(opening_brackets_stack[-1].position)
+    else:
+        print("Success")
 
 
 if __name__ == "__main__":
