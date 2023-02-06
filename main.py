01cda3d1f7be71e14d35507c3143717abb9d96eb
Algerts-Kligulis <124618375+Algerts-Kligulis@users.noreply.github.com>
2023-02-06 20:15:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def find_mismatch(text):
         if next in ")]}":
             # Process closing bracket, write your code here
             if len(opening_brackets_stack)==0 or not(are_matching(opening_brackets_stack[-1].char,next)):
-                print(i+1)
+                opening_brackets_stack.append(Bracket(next, i + 1))
                 return
             opening_brackets_stack.pop()
 
